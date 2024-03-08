865fb57455c9e0a87caf9873ee51172232fd5dfa
eldin001 <eldin.balje@icloud.com>
2024-03-08 14:10:34 +0100

    print numbers from 1 to 100

--- script.js
+++ script.js
@@ -12,3 +12,7 @@ Crea un container nel DOM , aggiungendo (attraverso la funzione append()) un ele
 *BONUS 2:*
 Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
 Se sei a corto di idee per lo stile, potresti prendere spunto dallo screenshot fornito in consegna. */
+
+for(let i = 1; i <= 100; i++) {
+    console.log(i);
+}
