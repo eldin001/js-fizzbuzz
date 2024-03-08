63a617e75f1ad2fca0858eb3430d88d9543f733d
eldin001 <eldin.balje@icloud.com>
2024-03-08 17:43:47 +0100

    final commit

--- script.js
+++ script.js
@@ -12,22 +12,32 @@ Crea un container nel DOM , aggiungendo (attraverso la funzione append()) un ele
 *BONUS 2:*
 Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
 Se sei a corto di idee per lo stile, potresti prendere spunto dallo screenshot fornito in consegna. */
+const squareWrapper = document.createElement('div');
+
+squareWrapper.className = 'd-flex justify-content-between align-content-center flex-wrap';
+squareWrapper.setAttribute('id','boxes');
 
 let numBox = 100;
 let tmpHtml = '';
 
-for (let i = 0; i < numBox; i++) {
+for (let i = 1; i <= numBox; i++) {
     if(i % 3 === 0 && i % 5 === 0) {
         tmpHtml +=  `<div class="fizzBuzz">${i} FizzBuzz</div>`
     } else if (i % 3 === 0) {
-        tmpHtml += ` <div class="fizz">${i} Fizz</div>`
+        tmpHtml += `<div class="fizz">${i} Fizz</div>`
     } else if (i % 5 === 0) {
-        tmpHtml += ` <div class="buzz">${i} Buzz</div>`
+        tmpHtml += `<div class="buzz">${i} Buzz</div>`
     } else {
         tmpHtml += `<div class="none">${i}</div>`
     }
 }
 
+squareWrapper.innerHTML = tmpHtml;
+
+console.dir(squareWrapper);
+
+const container = document.querySelector('.container');
+container.append(squareWrapper);
 
 
 
